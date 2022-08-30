e699b920e41da10b981156621fa39eb52c22acb2
khayratdinov <khayratdinov.np@gmail.com>
2022-08-31 01:09:38 +0500

    chore: Update models serializers in post

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,21 +1,27 @@
 from django.db import models
+# ============================================================================ #
 
+# ================================= CATEGORY ================================= #
 
 class Category(models.Model):
-    title = models.CharField(max_length=20, null=True, blank=True)
-    subTitle = models.CharField(max_length=10, null=True, blank=True)
-    
-    
-class Post(models.Model):
-    title = models.CharField(max_length=20, null=True, blank=True)
-    description = models.TextField(max_length=500, blank=True, null=True)
-    content = models.TextField(max_length=100, null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name='post')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        ordering = ['created_at']
-        
+    title = models.CharField(max_length=200)
+    sub_title = models.CharField(max_length=50, blank=True)
+    data_pub = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+# =================================== BLOG =================================== #
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    images = models.ImageField(upload_to="media", blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, related_name="blogs")
+    data_pub = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -1,24 +1,29 @@
-from .models import Post, Category
 from rest_framework import serializers
-        
+from .models import Category, Blog
+# ============================================================================ #
+
+# ============================== BLOGSERIALIZER ============================== #
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = ['id', 'title', 'description', 'body', 'images', 'category', 'data_pub', 'last_update']
+
+
+
+# ============================ CATEGORY SERIALZIER =========================== #
 class CategorySerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Category
-    fields = ['title', 'subTitle']
     
+    blogs_count = serializers.IntegerField(read_only=True)
     
-class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'sub_title', 'data_pub', 'blogs_count' ]
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+
+    blogs = BlogSerializer(many=True, read_only=True)
+    blogs_count = serializers.IntegerField(read_only=True)
   
-  class Meta:    
-    model = Post
-    fields = ["title", "description", "content", "category"]
-    extra_kwargs = {'category': {'read_only': True}}
-    
-  def get_title_category(self, category):
-    if category:
-      title = category.title
-    return title
-  
-  def get_category(self, category):
-    if category:
-      return self.category
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'sub_title', 'data_pub', 'blogs', 'blogs_count']
