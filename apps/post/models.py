d31f81c75db3b81f10de3d3a487997680886b07b
khayratdinov <khayratdinov.np@gmail.com>
2022-08-30 17:08:27 +0500

    chore: update post models, serialziers

--- apps/post/models.py
+++ apps/post/models.py
@@ -8,11 +8,14 @@ class Category(models.Model):
     
 class Post(models.Model):
     title = models.CharField(max_length=20, null=True, blank=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
     content = models.TextField(max_length=100, null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name='post')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
-        ordering = ['category']
+        ordering = ['created_at']
         
     def __str__(self):
         return self.title
--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -1,31 +1,22 @@
-import json
-from unicodedata import category
 from .models import Post, Category
 from rest_framework import serializers
-from django.contrib.postgres.fields import JSONField
         
 class CategorySerializer(serializers.ModelSerializer):
   class Meta:
     model = Category
-    fields = '__all__'
+    fields = ['title', 'subTitle']
     
-  
     
 class PostSerializer(serializers.ModelSerializer):
-  # category = serializers.SerializerMethodField("get_category")
-  # category = serializers.StringRelatedField(many=False)
-  # category = JSONField()
-  category = CategorySerializer(many=False)
   
   class Meta:    
     model = Post
-    fields = ("title", "content", "category_id", "category")
+    fields = ["title", "description", "content", "category"]
     extra_kwargs = {'category': {'read_only': True}}
     
   def get_title_category(self, category):
     if category:
       title = category.title
-    
     return title
   
   def get_category(self, category):
