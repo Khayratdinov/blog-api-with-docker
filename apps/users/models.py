8e908abc177d66b018a45db00d7af0066548786a
khayratdinov <khayratdinov.np@gmail.com>
2022-09-01 21:12:48 +0500

    chore: Update models in users app

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+# ============================================================================ #
 
-# Create your models here.
+
+# =================================== USER =================================== #
+class User(AbstractUser):
+
+    username = models.CharField(
+        'Login',
+        max_length=150,
+        unique=True,
+        help_text='Nick name',
+        validators=[
+            RegexValidator(
+                regex=r'^[\w.@+-]+',
+                message=('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.')
+            )
+        ]
+    )
+    email = models.EmailField(max_length=254,unique=True,)
+    first_name = models.CharField(max_length=150,blank=True,null=True,)
+    last_name = models.CharField(max_length=150,blank=True,null=True,)
+    confirmation_code = models.CharField(max_length=255,null=True,blank=False,)
+    bio = models.TextField(
+                            blank=True,
+                            null=True,
+                            help_text='User biography',
+    )
+    class Meta:
+        ordering = ('username',)
+
+    def __str__(self) -> str:
+        return self.username
