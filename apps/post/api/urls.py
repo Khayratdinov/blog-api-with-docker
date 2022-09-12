a2e278e8de6d915215c2ebc8624eb03b8115d872
khayratdinov <khayratdinov.np@gmail.com>
2022-09-12 19:06:43 +0500

    chore: Post views with ModelsViewSet

--- apps/post/api/urls.py
+++ apps/post/api/urls.py
@@ -1,14 +1,27 @@
-from django.urls import path
+from django.urls import path, include
+# ============================================================================ #
+from rest_framework_nested import routers
+# ============================================================================ #
 from apps.post.api import views
 
+router = routers.DefaultRouter()
+
+router.register(r'blog', views.BlogViewSet, basename='blog')
+
+
+
+
+
 urlpatterns = [
-    path('blogs/', views.BlogList.as_view()),
-    path('blogs/<int:pk>/', views.BlogDetail.as_view()),
+
     path('author-blogs/<str:username>/', views.BlogListByAuthor.as_view()),
 
 
     path('category-with-class/<int:pk>/', views.CategoryDetail.as_view()),
     path('category-with-function/', views.category_list),
     path('category-with-function/<int:pk>/', views.category_detail),
+
     
 ]
+
+urlpatterns  += router.urls
--- apps/post/api/views.py
+++ apps/post/api/views.py
@@ -2,13 +2,15 @@
 
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 # ============================================================================ #
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes as view_permission_classes
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework import status
-from rest_framework.permissions import  IsAuthenticatedOrReadOnly
+from rest_framework.permissions import  IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 # ============================================================================ #
 from apps.common.permissions import IsSuperUserOrAuthorOrReadOnly, IsSuperUserOrReadOnly
 
@@ -24,32 +26,27 @@ from apps.post.pagination import DefaultPagination
 # ============================================================================ #
 
 
-class BlogList(ListCreateAPIView):
-    """
-    get:
-        Barcha mavjud bloglar royxatini qaytaradi.
-    """
-    
+
+class BlogViewSet(ModelViewSet):
+
     serializer_class = BlogSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly,]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ['title', 'description']
-    ordering_fields = ['data_pub',]
+    ordering_fields = ['created_at',]
     pagination_class = DefaultPagination
+
 
     def get_queryset(self):
         return Blog.objects.select_related_object('category')
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsSuperUserOrAuthorOrReadOnly()]
+        return [IsAuthenticatedOrReadOnly()]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
-class BlogDetail(RetrieveUpdateDestroyAPIView):
-    serializer_class = BlogSerializer
-    permission_classes = [IsSuperUserOrAuthorOrReadOnly,]
-
-    def get_queryset(self):
-        return Blog.objects.select_related_object('category')
 
 
 
@@ -64,13 +61,9 @@ class BlogListByAuthor(ListAPIView):
         return Blog.objects.filter(author__username=self.kwargs['username'])
 
 
-
 # ============================================================================ #
 #                                   CATEGORY                                   #
 # ============================================================================ #
-
-
-# ================================ CLASS VIEWS =============================== #
 
 
 class CategoryDetail(RetrieveUpdateDestroyAPIView):
@@ -80,44 +73,87 @@ class CategoryDetail(RetrieveUpdateDestroyAPIView):
 
 
 
+# ============================================================================ #
+#                                   OLD CODE                                   #
+# ============================================================================ #
+
+
+
+# ============================================================================ #
+#                                   CATEGORY                                   #
+# ============================================================================ #
 
 # ============================== FUNCTION VIEWS ============================== #
 
 
-@api_view(['GET', 'POST'])
-def category_list(request):
-    if request.method == 'GET':
-        category = Category.objects.annotate(blogs_count=Count('blogs')).all()
-        serializer = CategorySerializer(category, many=True, context={'request': request})
-        return Response(serializer.data)
+# @api_view(['GET', 'POST'])
+# def category_list(request):
+#     if request.method == 'GET':
+#         category = Category.objects.annotate(blogs_count=Count('blogs')).all()
+#         serializer = CategorySerializer(category, many=True, context={'request': request})
+#         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = CategorySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def category_detail(request, pk):
-    category = Category.objects.get(id=pk)
-
-    if request.method == 'GET':
-        serializers = CategoryDetailSerializer(category)
-        return Response(serializers.data)
-
-    elif request.method == 'PUT':
-        serializers = CategoryDetailSerializer(category, data=request.data)
-        serializers.is_valid(raise_exception=True)
-        serializers.save()
-        return Response(serializers.data)
-
-    elif request.method == 'DELETE':
-        if category.blogs.count() > 0:
-            return Response({'error': 'Categoryga bogliq bloglar borligi sabab buni oshira olmaysiz'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == 'POST':
+#         serializer = CategorySerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def category_detail(request, pk):
+#     category = Category.objects.get(id=pk)
+
+#     if request.method == 'GET':
+#         serializers = CategoryDetailSerializer(category)
+#         return Response(serializers.data)
+
+#     elif request.method == 'PUT':
+#         serializers = CategoryDetailSerializer(category, data=request.data)
+#         serializers.is_valid(raise_exception=True)
+#         serializers.save()
+#         return Response(serializers.data)
+
+#     elif request.method == 'DELETE':
+#         if category.blogs.count() > 0:
+#             return Response({'error': 'Categoryga bogliq bloglar borligi sabab buni oshira olmaysiz'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+#         category.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+
+# ============================================================================ #
+#                                     BLOG                                     #
+# ============================================================================ #
+
+# class BlogList(ListCreateAPIView):
+#     """
+#     GET:
+#         Barcha mavjud bloglar royxatini qaytaradi.
+    
+#     POST:
+#          Royxattan otkan foydalanuvshi Blog yarata oladi
+
+#     """
+    
+#     serializer_class = BlogSerializer
+#     permission_classes = [IsAuthenticatedOrReadOnly,]
+#     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+#     search_fields = ['title', 'description']
+#     ordering_fields = ['data_pub',]
+#     pagination_class = DefaultPagination
+
+#     def get_queryset(self):
+#         return Blog.objects.select_related_object('category')
+
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
+
+
+
+# class BlogDetail(RetrieveUpdateDestroyAPIView):
+#     serializer_class = BlogSerializer
+#     permission_classes = [IsSuperUserOrAuthorOrReadOnly,]
+
+#     def get_queryset(self):
+#         return Blog.objects.select_related_object('category')
